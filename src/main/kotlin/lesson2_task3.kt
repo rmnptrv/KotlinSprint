f80bbdb5c8d2322d5f9e9331a39bacdefa0aa528
LoL1k <Petrov.roman.1@yandex.ru>
2024-03-12 20:38:10 +0300

    KS-2-3 buff with const problem

--- src/main/kotlin/lesson2_task3.kt
+++ src/main/kotlin/lesson2_task3.kt
@@ -2,13 +2,15 @@ package org.example
 
 fun main () {
 
+    const val minute = 60
     val startHour = 9
     val startMinute = 39
-    val startTime = (startHour * 60) + startMinute
     val travelTime = 457
+
+    val startTime = (startHour * minute) + startMinute
     val finishTime = startTime + travelTime
-    val finishHour = finishTime / 60
-    val finishMinute = finishTime % 60
+    val finishHour = finishTime / minute
+    val finishMinute = finishTime % minute
 
     print(finishHour)
     print(":")
