a8eb2073400644984b90cb729785403bac2c0eec
LoL1k <Petrov.roman.1@yandex.ru>
2024-03-12 10:22:22 +0300

    KS-1-3 space correct new

--- src/main/kotlin/lesson_1/lesson1_task3.kt
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -3,18 +3,15 @@ package org.example.lesson_1
 fun main () {
 
     val year = 1961
-    var hour = 9
-    var minute = 7
-    var zero = 0
+    var hour = "09"
+    var minute = "07"
 
     println(year)
-    //print(zero)
     //println(hour)
-    //print(zero)
     //println(minute)
 
-    hour = 10
-    minute = 55
+    hour = "10"
+    minute = "55"
 
     print(hour)
     print(":")
